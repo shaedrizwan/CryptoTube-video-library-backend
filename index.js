e657a462c08cb061c136c9b047197020637d2709
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-06-11 00:18:54 +0530

    test

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.get('/test',(req,res)=>{
+  res.json({success:true,message:"This is a test route"})
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}!`)
 });
